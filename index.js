4cccd1a16157b504c2788b64551d6c677d9d45bd
a.tovtk@gmail.com <a.tovtk@gmail.com>
2016-05-24 14:28:04 +0300

    Fb webhook

--- index.js
+++ index.js
@@ -9,6 +9,15 @@ app.get('/', function (req, res) {
   res.send('Hello World!');
 });
 
+// Node.js Example
+app.get('/webhook', function (req, res) {
+  if (req.query['hub.verify_token'] === 'EAAWOXoAWUzcBAHdpf1QIeH4VIiZAMFPdhbEX4norAlQDdRHcOLS9mV5KSnrejONw5JFeUJABJjzqHCirjRsoj998kD5GaT5hdxTifaEkQfz2iHgnLYdCuT4Bay7ZBK4eN6xtZCBWtZASYNdc8d4NOYvqIIjubKYjKzZAZAxE1ZAvAZDZD') {
+    res.send(req.query['hub.challenge']);
+  } else {
+    res.send('Error, wrong validation token');    
+  }
+});
+
 app.listen(port, function() {
     console.log('Our app is running on http://localhost:' + port);
 });
