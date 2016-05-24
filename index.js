ffff272e8de508b5ed112ca9e6e0850e942d149c
a.tovtk@gmail.com <a.tovtk@gmail.com>
2016-05-24 15:59:20 +0300

    post

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ var port = process.env.PORT || 8080;
 
 app.get('/', function (req, res) {
   res.send('Hello World!');
-  console.log('Log test 2');
+  console.log('Home Event');
 });
 
 // Node.js Example
@@ -17,9 +17,25 @@ app.get('/webhook', function (req, res) {
   } else {
     res.send('Error, wrong validation token');    
   }
-  console.log('Log test 3');
-  console.log(request.body);
+  console.log('Get Event');
+  //console.log(request.body);
   
+});
+
+// handler receiving messages
+app.post('/webhook', function (req, res) {
+    /*var events = req.body.entry[0].messaging;
+    for (i = 0; i < events.length; i++) {
+        var event = events[i];
+        if (event.message && event.message.text) {
+            sendMessage(event.sender.id, {text: "Echo: " + event.message.text});
+        }
+    }*/
+	console.log('Post Event');
+	console.log(request.body);
+	console.log(req.body.entry[0].messaging);
+	console.log(req.body.entry[0].messaging[0]);
+    res.sendStatus(200);
 });
 
 app.listen(port, function() {
